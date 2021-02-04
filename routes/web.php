7876d7079e56f4089ff98fa7a6dcb901de548d92
PhuongNgan2103 <nguyenphuongngan2103@gmail.com>
2021-02-04 17:30:09 +0700

    task_management

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
+
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,19 +18,50 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+Route::prefix('customer')->group(function () {
+    Route::get('index', function () {
+        // Hiển thị danh sách khách hàng
+        return view('modules.customer.index');
+    });
+
+    Route::get('create', function () {
+        // Hiển thị Form tạo khách hàng
+    });
+
+    Route::post('store', function () {
+        // Xử lý lưu dữ liệu tạo khách hàng thong qua phương thức POST từ form
+    });
+
+    Route::get('{id}/show', function () {
+        // Hiển thị thông tin chi tiết khách hàng có mã định danh id
+    });
+
+    Route::get('{id}/edit', function () {
+        // Hiển thị Form chỉnh sửa thông tin khách hàng
+    });
+
+    Route::patch('{id}/update', function () {
+        // xử lý lưu dữ liệu thông tin khách hàng được chỉnh sửa thông qua PATCH từ form
+    });
+
+    Route::delete('{id}', function () {
+        // Xóa thông tin dữ liệu khách hàng
+    });
 });
 
+Route::resource('customers', CustomerController::class);
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 
-//tao group route tasks
 Route::group(['prefix' => 'tasks'], function () {
-    Route::get('/',[TaskController::class , 'index'])->name('tasks.index');
-    Route::get('/create',[TaskController::class , 'create'])->name('tasks.create');
-    Route::post('/create',[TaskController::class , 'store'])->name('tasks.store');
-    Route::get('/{id}/edit',[TaskController::class , 'edit'])->name('tasks.edit');
-    Route::post('/{id}/edit',[TaskController::class , 'update'])->name('tasks.update');
-    Route::get('/{id}/destroy',[TaskController::class , 'destroy'])->name('tasks.destroy');
+    Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/create', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::post('/{id}/edit', [TaskController::class, 'update'])->name('tasks.update');
+    Route::get('/{id}/destroy', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
